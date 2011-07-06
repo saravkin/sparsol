4e11ec937f78d69b7f5dd23148148e345c2314aa
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2011-07-06 13:06:51 -0700

    Revised test to show why Huber is a good thing to do

--- examples/HuberTest.m
+++ examples/HuberTest.m
@@ -3,24 +3,24 @@
 m = 120; n = 512; k = 20; % m rows, n cols, k nonzeros.
 p = randperm(n); x0 = zeros(n,1); x0(p(1:k)) = sign(randn(k,1));
 A  = randn(m,n); [Q,R] = qr(A',0);  A = Q';
-b  = A*x0 + 0.01 * randn(m,1);
+b  = A*x0 + 0.005 * randn(m,1);
 tau = norm(x0,1);
 
 %% Subproblem options
-options.vapnikEps = 1e-7;
+options.vapnikEps = 0;
 options.lassoOpts.optTol = 1e-10;
 options.lassoOpts.verbosity = 0;
 options.tolerance = 1e-7*norm(b);
 options.primal = 'lsq';
 options.exact  = 1;
 
-%s = 3;
-%Err = zeros(m, 1); % outliers
-%pErr = randperm(m);
-%Err(pErr(1:s)) = randn(s, 1);
-%b = b + Err;
+s = 5; % number of outliers
+Err = zeros(m, 1); % outliers
+pErr = randperm(m);
+Err(pErr(1:s)) = .5*randn(s, 1); % outliers are 100 times the size
+b = b + Err;
 
-sigma = 1e-4;
+sigma = 1e-2;
 
 
 %%
@@ -33,8 +33,8 @@ fprintf('Target tau = %15.7e\n', tau);
 options.primal = 'huber';
 %options.hparaM = 1e-3;
 %options.hparaT = 1e2;
-options.hparaM = .005;
-sigma = 1;
+options.hparaM = 0.005;
+sigma = 1e-2;
 options.rootFinder = 'newton';
 [xHuber,info] = gbpdn(A, b, 0, sigma, [], options); 
 fprintf('Target tau = %15.7e\n', tau);
@@ -47,6 +47,6 @@ plot(1:n, x0, 1:n, xL2(1:n) -2, 1:n, xHuber(1:n) + 2);legend('true', 'l2', 'hube
 
 
 figure(2)
-plot(1:m, b - A*x0, 1:m, b - A*xL2(1:n)+.05, 1:m, b - A*xHuber(1:n)-.05);legend('True Outliers', 'l2 residuals', 'Huber Residuals')
+plot(1:m, b - A*x0, 1:m, b - A*xL2(1:n)+3, 1:m, b - A*xHuber(1:n)-3);legend('True Outliers', 'l2 residuals', 'Huber Residuals')
 
 
